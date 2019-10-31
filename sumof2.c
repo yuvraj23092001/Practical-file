434c23c7994573c680bcb2f85afcefc8947c2670
yuvraj23092001 <54389501+yuvraj23092001@users.noreply.github.com>
2019-10-31 11:39:02 +0530

    Add files via upload

---
+++ sumof2.c
@@ -0,0 +1,12 @@
+// to find sum of two numbers
+#include<stdio.h>
+int main()
+{
+float m,n,result;
+printf("enter two numbers to be added");
+scanf("%f %f",&m,&n);
+
+result=m+n;
+printf("sum of two numbers is %.2f",result);
+return 0;
+}
