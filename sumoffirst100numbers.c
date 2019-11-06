2fac69aae2077949e0429958e3ff1bb4aaecd2c5
yuvraj23092001 <54389501+yuvraj23092001@users.noreply.github.com>
2019-11-06 13:53:04 +0530

    Create sumoffirst100numbers.c

---
+++ sumoffirst100numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>  
+int main()  
+{  
+int i,sum=0;  
+for(i=1;i<=100;i++)  
+{                                                                         
+sum=sum+i;  
+}  
+printf("Sum of first 100 numbers is %d \n",sum);  
+return 0;  
+}  
