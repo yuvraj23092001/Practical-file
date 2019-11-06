0d9616ba8826b603c9564ebc198d4a8dd60663d8
yuvraj23092001 <54389501+yuvraj23092001@users.noreply.github.com>
2019-11-06 14:12:54 +0530

    Create greatestoftwo.c

---
+++ greatestoftwo.c
@@ -0,0 +1,16 @@
+#include<stdio.h>  
+int main()  
+{  
+int a,b;  
+printf("Enter the  value of a and b");  
+scanf("%d%d",&a,&b);  
+if(a>b)  
+{  
+printf("a is grteater");  
+}  
+else  
+{  
+printf("b is greater");  
+}                                                                         
+return 0;  
+}  
